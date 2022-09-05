e7e6c193a356b82704975cdb97ecc241c5fc1ec5
Lee KJ <112466589+lkjae@users.noreply.github.com>
2022-09-05 17:46:23 +0900

    Add files via upload

---
+++ Quiz02/Quiz02/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox1.Text.Substring(0, 3);
+            textBox3.Text = textBox1.Text.Substring(textBox1.Text.Length - 3,3);
+            textBox4.Text = textBox1.Text.Substring(5, 3);
+            textBox5.Text = textBox1.TextLength.ToString();
+        }
+    }
+}
---
+++ Quiz04/Quiz04/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num;
+            double sum_Odd = 0, sum_Even = 0;
+            num = int.Parse(textBox1.Text);
+
+            textBox2.Text = "";
+            textBox3.Text = "";
+
+            for(int i = 1; i <= num; ++i)
+            {
+                if(i % 2 == 0)
+                {
+                    sum_Even += i;
+                    textBox3.Text = textBox3.Text + i + "+";
+                }
+                else
+                {
+                    sum_Odd += i;
+                    textBox2.Text = textBox2.Text + i + "+";
+                }
+
+            }
+            textBox2.Text = textBox2 .Text + "=" + sum_Odd;
+            textBox3.Text = textBox3.Text + "=" + sum_Even;
+        }
+    }
+}
