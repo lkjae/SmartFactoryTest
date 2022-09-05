accb40eebdd405b82c379f0aa54ad2650ec77c4a
Lee KJ <112466589+lkjae@users.noreply.github.com>
2022-09-05 17:48:12 +0900

    Add files via upload

---
+++ Quiz05/Quiz05/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile("C:\\Temp\\vancouver.PNG");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile("C:\\Temp\\tomyx.PNG");
+        }
+    }
+}
